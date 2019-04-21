1e28755cea2c1aae08ee956e778a8aea0bef1207
ishibshi.futos <ishibshi.futos@outlook.com>
2019-04-21 16:56:05 +0900

    add new files

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public GameObject masterObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        masterObject.GetComponent<GameMaster>().boxNum--;
+        Destroy(gameObject);
+    }
+}
